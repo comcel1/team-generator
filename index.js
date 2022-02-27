5b2fb5d4a2d3c448febc672b475b9ebb9c68da24
PaulNelsonOld <90969624+comcel1@users.noreply.github.com>
2022-02-27 12:17:00 -0600

    installed profile dependencies, corrected misspelling in Manager.js filename

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+const generateHTML = require('./src/generateHTML');
+
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('./dist');
+
+// add team profile dependencies
+const Manager = require('./lib/Manager')
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern')
+const Engineer = require('./lib/Engineer')
+
